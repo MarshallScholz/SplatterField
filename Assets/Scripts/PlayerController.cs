a9559dc4f66d873a06c70d16534abd022fc22902
Marshall Scholz <marshall.scholz@gmail.com>
2022-04-13 22:19:05 +0930

    Added smooth animation transitions for player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    public float smooth;
     void Update()
     {
         float fwd = Input.GetAxis("Vertical");
-        animator.SetFloat("Forward", Mathf.Abs(fwd));
-        animator.SetFloat("Sense", Mathf.Sign(fwd));
-        animator.SetFloat("Turn", Input.GetAxis("Horizontal"));
+        animator.SetFloat("Forward", Mathf.Abs(fwd), smooth, Time.deltaTime);
+        animator.SetFloat("Sense", Mathf.Sign(fwd), smooth, Time.deltaTime);
+        animator.SetFloat("Turn", Input.GetAxis("Horizontal"), smooth, Time.deltaTime);
 
     }
 
