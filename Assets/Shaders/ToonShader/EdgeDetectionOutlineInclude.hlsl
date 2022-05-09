46df05976430a36b8d84630274dcf30dcb9d7f14
Marshall_Scholz <s181125@students.aie.edu.au>
2022-05-09 15:01:38 +0930

    Cleaned up assets more. Worked on edge detection a little. Will need to look at the Blur tutorial for HDRP post processing to get a better understanding https://aie.instructure.com/courses/809/pages/Computer%20Graphics%2021%20-%20Unity%20Post-Processing?titleize=0

--- Assets/Shaders/ToonShader/EdgeDetectionOutlineInclude.hlsl
+++ Assets/Shaders/ToonShader/EdgeDetectionOutlineInclude.hlsl
@@ -1,15 +1,12 @@
 #ifndef SOBELOUTLINES_INCLUDED
 #define SOBELOUTLINES_INCLUDED
 
-#include "UnityCG.cginc"
-
 //sample from relative pixel (0, 0)
 static float2 sobelSamplePoints[9] =
 {
-	float2(-1, 1_, float2(0, 1), float2(1, 1),
-	float2(-1, 0, float2(0, 0), float2(1, 1),
-	float2(-1, -1_, float2(0, -1), float2(1, -1)
-
+	float2(-1, 1), float2(0, 1), float2(1, 1),
+	float2(-1, 0), float2(0, 0), float2(1, 1),
+	float2(-1, -1), float2(0, -1), float2(1, -1)
 };
 
 //Weights for x component
@@ -30,13 +27,13 @@ static float sobelYMatrix[9] =
 
 //Runs sobel algorithm over the depth texture
 //			'_float' tells the shader what type of numbers it uses.
-void DepthSobel_float(float2 UV, float Thickness, out float Out)
+void DepthSobel_float(float2 UV, float Thickness,  SceneDepthTexture, out float Out)
 {
 	float sobel = 0;
 	
 	[unroll] for (int i = 0; i < 9; i++)
 	{
-		float depth = Sampler depthTextureMode(UV + sobelSamplePoints[i] * Thickness);
+		float depth = LOAD_TEXTURE2D_LOD(SceneDepthTexture,UV + sobelSamplePoints[i] * Thickness, 0);
 		//float depth = _CameraDepthTexutre(UV + sobelSamplePoints[i] * Thickness);
 					//multiply the depths by the weights
 		sobel += depth * float2(sobelXMatrix[i], sobelYMatrix[i]);
